ea5f69c7c7401e0fd85ee140dc6bab8cba0d8fa4
Vo Van Nam <vovannam220304@gmail.com>
2024-09-20 22:23:55 +0700

    khoi tao project

---
+++ src/main/java/Controllers/HelloWord.java
@@ -0,0 +1,20 @@
+package Controllers;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet (urlPatterns ={"/home"})
+public class HelloWord extends HttpServlet{		
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher rd = req.getRequestDispatcher("/home.jsp");
+		rd.forward(req, resp);
+	}
+}
---
+++ src/main/webapp/home.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action ="home" method="get">
+		<label>Nhập catename:</label>
+		<input type="text"  name ="cateName">
+		<label>Nhập link hình :</label>
+		<input type  ="text" name="images">
+		<input type ="submit" value ="Add Category">
+	 </form>
+</body>
+</html>
